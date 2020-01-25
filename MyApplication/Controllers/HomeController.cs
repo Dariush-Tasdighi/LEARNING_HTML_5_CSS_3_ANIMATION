77d19bc9bffcc0c11a15789e3610c7c1eaee631d
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2020-01-25 03:45:21 +0330

    Add project files.

---
+++ MyApplication/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿//using System.Linq;
+
+namespace MyApplication.Controllers
+{
+	public partial class HomeController : Infrastructure.BaseController
+	{
+		public HomeController() : base()
+		{
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Index()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0100()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0200()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0300()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0400()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0500()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0600()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0700()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0800()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_0900()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1000()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1100()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1200()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1300()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1400()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1500()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1600()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1700()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1800()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_1900()
+		{
+			return View();
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Learn_2000()
+		{
+			return View();
+		}
+	}
+}
---
+++ MyApplication/Global.asax.cs
@@ -0,0 +1,15 @@
+namespace MyApplication
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		public MvcApplication() : base()
+		{
+		}
+
+		protected void Application_Start()
+		{
+			System.Web.Mvc.AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(System.Web.Routing.RouteTable.Routes);
+		}
+	}
+}
